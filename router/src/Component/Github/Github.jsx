a3c2f0d52b86483182b373021071a98b97ee9f17
vishnu <vishnuchudhary2020@gmail.com>
2025-11-05 19:42:11 +0530

    a project using react and handle api

---
+++ router/src/Component/Github/Github.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { data } from "react-router-dom";
+function Github(){
+ const[data,setData]=useState([0])
+    useEffect(()=>{
+        fetch('https://api.github.com/users/aanandmohan')
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data);
+            setData(data);
+        })
+    },[])
+    return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  )
+}
+
+export default Github
